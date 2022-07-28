3c0c76258e2792a6c2fef446b978c532a6148381
YoussefWilliam <youssef.william997@gmail.com>
2022-07-28 19:44:38 +0200

    Finish normal calculator using ruby

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
+ puts "Simple calculator"
+
+ 20.times {print "*"}
+
+ puts
+ puts "Enter your first number"
+ num_1 = gets.chomp
+
+ puts "Enter your second number"
+ num_2 = gets.chomp
  
+puts "Adding these numbers is: #{num_1.to_i + num_2.to_i}"
+puts "Subtracting these numbers is: #{num_1.to_i - num_2.to_i}"
+puts "Multiplying these numbers is: #{num_1.to_i * num_2.to_i}"
+puts "Dividing these numbers is: #{num_1.to_f / num_2.to_f}"
+puts "Modulus these numbers is: #{num_1.to_i % num_2.to_i}"
